b7580b9a8ac4720042127e11a486f1cd36b4b673
Cuboid <fanjas112358@gmail.com>
2025-07-10 18:18:15 -0400

    optimisation
    if the container is already correct don't bother messing about
    also remove debug calls

--- background.js
+++ background.js
@@ -1,33 +1,33 @@
 function handleTabCreated(tab) {
     // goofy heuristic to determine new tabs
-    console.log(tab);
     if ((tab.title === "New Tab") && (tab.status === "complete")) {
         browser.tabs.query({groupId: tab.groupId, windowId: tab.windowId}).then((tabs) => {
-            console.log(tabs);
             // find leftmost tab by searching for lowest index
             let firstTab = tabs.reduce((acc, cur) => ((cur.index < acc.index) ? cur : acc));
 
-            // remove the listener to lock it and prevent an infinite loop
-            browser.tabs.onCreated.removeListener(handleTabCreated);
-            // this little chunk of code partially taken from mozilla's multi-account-containers addon
-            // basically duplicate the current tab with the first tab's container
-            browser.tabs.create({
-                // omit the url so it defaults to new tab
-                cookieStoreId: firstTab.cookieStoreId,
-                index: tab.index,
-                active: tab.active,
-                openerTabId: tab.openerTabId,
-                windowId: tab.windowId
-            }).then((newTab) => {
-                // put the listener back
-                browser.tabs.onCreated.addListener(handleTabCreated);
-                // remove the old tab that has the incorrect container
-                browser.tabs.remove(tab.id);
-                // and then set its group correctly
-                if (tab.groupId !== -1) {
-                    browser.tabs.group({groupId: tab.groupId, tabIds: newTab.id});
-                }
-            });
+            if (tab.cookieStoreId !== firstTab.cookieStoreId) {
+                // remove the listener to lock it and prevent an infinite loop
+                browser.tabs.onCreated.removeListener(handleTabCreated);
+                // this little chunk of code partially taken from mozilla's multi-account-containers addon
+                // basically duplicate the current tab with the first tab's container
+                browser.tabs.create({
+                    // omit the url so it defaults to new tab
+                    cookieStoreId: firstTab.cookieStoreId,
+                    index: tab.index,
+                    active: tab.active,
+                    openerTabId: tab.openerTabId,
+                    windowId: tab.windowId
+                }).then((newTab) => {
+                    // put the listener back
+                    browser.tabs.onCreated.addListener(handleTabCreated);
+                    // remove the old tab that has the incorrect container
+                    browser.tabs.remove(tab.id);
+                    // and then set its group correctly
+                    if (tab.groupId !== -1) {
+                        browser.tabs.group({groupId: tab.groupId, tabIds: newTab.id});
+                    }
+                });
+            }
         })
     }
 }
